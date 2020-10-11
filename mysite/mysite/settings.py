cd39d20188740e844fe9b52006125ec92569f926
ellinx <ellin.xll@gmail.com>
2020-10-10 23:42:10 -0700

    Change settings.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xy+r(4_$bt(y=tod2@&1mbdl768b1snp8=mb7=r3fwoxh3m0ot'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ellin.pythonanywhere.com']
+ALLOWED_HOSTS = ['ellin.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
